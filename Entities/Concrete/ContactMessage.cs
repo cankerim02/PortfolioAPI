c02d8e51f1b6e0683781d91debf71b02ed526d2f
cankerim02 <cankerim02@gmail.com>
2025-07-19 16:07:16 +0300

    Add project files.

---
+++ Entities/Concrete/ContactMessage.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class ContactMessage : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? AdminReply { get; set; }
+        public DateTime? RepliedAt { get; set; }
+        public bool IsReplied => AdminReply != null;
+    }
+}
---
+++ Entities/Dtos/ContactMessageDto.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class ContactMessageDto : IDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
