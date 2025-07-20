53884f805ece033d326c9d8926685701733994e5
cankerim02 <cankerim02@gmail.com>
2025-07-20 21:04:00 +0300

    Projeler için varlık katmanı oluşturuldu.

---
+++ Entities/Concrete/Project.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Project : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string ProjectUrl { get; set; }
+    }
+}
