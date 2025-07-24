05d50acaca1cd3998ca2c4473f3c27eacc61f8f9
cankerim02 <cankerim02@gmail.com>
2025-07-24 20:44:27 +0300

    ContactMessage için metot imzaları oluşturuldu.

---
+++ DataAccess/Abstract/IContactMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IContactMessageRepository 
+    {
+        Task<List<ContactMessage>> GetAllContactAsync();
+        Task<ContactMessage> GetByIdContactAsync(int id);
+        Task AddContactAsync(ContactMessage contactMessage);
+        Task UpdateContactAsync(ContactMessage contactMessage);
+        Task DeleteContactAsync(int id);
+
+    }
+}
