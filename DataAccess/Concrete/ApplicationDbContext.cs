47e3e68ac0765cbe9426f01d6c9052f4c8339959
cankerim02 <cankerim02@gmail.com>
2025-07-24 20:48:16 +0300

    EFcore paketleri yüklendi ve veritabanı tabloları oluşturuldu.

---
+++ DataAccess/Concrete/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+
+    }
+}
