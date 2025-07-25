f405a464e49948da706d3efa942b99c933250368
cankerim02 <cankerim02@gmail.com>
2025-07-26 01:11:50 +0300

    ProjectRepo için interface oluşturuldu ve metot imzaları tanımlandı.

---
+++ DataAccess/Abstract/IProjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProjectRepository
+    {
+        Task<List<Project>> GetAllProjectAsync();
+        Task<Project?> GetProjectByIdAsync(int id);
+        Task AddProjectAsync(Project project);
+        Task UpdateProjectAsync(Project project);
+        Task DeleteProjectAsync(int id);
+    }
+}
