eceae77aba441199b42735ed264d93ccf12726a4
cankerim02 <cankerim02@gmail.com>
2025-07-26 01:12:44 +0300

    Project repo sınıfı oluşturuldu ve interface miras alınıp gerekli veritabanı işlemleri yapıldı.

---
+++ DataAccess/Concrete/EfProjectRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class EfProjectRepository : IProjectRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EfProjectRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddProjectAsync(Project project)
+        {
+            await _context.Projects.AddAsync(project);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProjectAsync(int id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+            if (project != null)
+            {
+                _context.Projects.Remove(project);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Project>> GetAllProjectAsync()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+
+        public async Task<Project> GetProjectByIdAsync(int id)
+        {
+            return await _context.Projects.FindAsync(id);
+        }
+
+        public async Task UpdateProjectAsync(Project project)
+        {
+            _context.Projects.Update(project);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
