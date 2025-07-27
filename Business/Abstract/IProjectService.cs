2328baa88bd0bc1fbbfa6feed50e77db3bb391fb
cankerim02 <cankerim02@gmail.com>
2025-07-27 12:59:13 +0300

    Projeler için servis arayüzü oluşturulup metot imzaları belirlendi

---
+++ Business/Abstract/IProjectService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProjectServices
+    {
+        Task<List<Project>> GetAllAsync();
+        Task<Project?> GetByIdAsync(int id);
+        Task AddAsync(Project project);
+        Task UpdateAsync(Project project);
+        Task DeleteAsync(int id);
+    }
+}
