a12043f32887fe601838906c8f5b934ec34fa26a
cankerim02 <cankerim02@gmail.com>
2025-07-28 21:03:39 +0300

    Email Service için  yazılan arayüzün iş kuralları business katmanında yazıldı.

---
+++ Business/Concrete/EmailServicesManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace Business.Concrete
+{
+    public class EmailServicesManager : IEmailServices
+    {
+        private readonly IConfiguration _config;
+
+        public EmailServicesManager(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient(_config["Smtp:Host"])
+            {
+                Port = int.Parse(_config["Smtp:Port"]),
+                Credentials = new NetworkCredential(_config["Smtp:Username"], _config["Smtp:Password"]),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_config["Smtp:From"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = false
+            };
+
+            mailMessage.To.Add(toEmail);
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
