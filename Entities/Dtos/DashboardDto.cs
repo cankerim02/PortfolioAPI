df7920b7ba29d65685bc8df79efae28eefd90ac2
cankerim02 <cankerim02@gmail.com>
2025-08-02 16:28:03 +0300

    admin panelinde toplam proje, mesaj ve yanıtlanan mesajları görmek için bir dto sınıfı oluşturuldu.

---
+++ Entities/Dtos/DashboardDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+   
+    public class DashboardDto
+    {
+        public int TotalProjects { get; set; }
+        public int TotalMessages { get; set; }
+        public int RepliedMessages { get; set; }
+    }
+
+}
