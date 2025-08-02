6ca24c4e11812f9f12d619652f432edf0b6db45f
cankerim02 <cankerim02@gmail.com>
2025-08-02 16:32:29 +0300

    dashboard repo sınıfı oluşturuldu ve arayüzden metotlar kalıtım yoluyla implement edilerek gerekli işlemler yapıldı.

---
+++ DataAccess/Concrete/DashboardRepository.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> GetTotalProjectsAsync()
+        {
+            return await _context.Projects.CountAsync();
+        }
+
+        public async Task<int> GetTotalMessagesAsync()
+        {
+            return await _context.ContactMessages.CountAsync();
+        }
+
+        public async Task<int> GetRepliedMessagesAsync()
+        {
+            return await _context.ContactMessages.Where(m => m.AdminReply !=null).CountAsync();
+        }
+    }
+
+}
