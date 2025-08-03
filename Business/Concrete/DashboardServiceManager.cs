0a6cc1087181a8f80701292134fe75b22de678cc
cankerim02 <cankerim02@gmail.com>
2025-08-03 13:50:17 +0300

    Dashboard arayüzü kalıtım yoluyla manager sınıfına entegre edildi ve gerekli istatistikler gösterilmesi  dashboard repodan veriler çekildi.

---
+++ Business/Concrete/DashboardServiceManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DashboardServiceManager : IDashboardService
+    {
+        private readonly IDashboardRepository _dashboardRepository;
+
+        public DashboardServiceManager(IDashboardRepository dashboardRepository)
+        {
+            _dashboardRepository = dashboardRepository;
+        }
+
+        public async Task<DashboardDto> GetDashboardDataAsync()
+        {
+            var totalProjects = await _dashboardRepository.GetTotalProjectsAsync();
+            var totalMessages = await _dashboardRepository.GetTotalMessagesAsync();
+            var repliedMessages = await _dashboardRepository.GetRepliedMessagesAsync();
+
+            return new DashboardDto
+            {
+                TotalProjects = totalProjects,
+                TotalMessages = totalMessages,
+                RepliedMessages = repliedMessages
+            };
+        }
+    }
+}
