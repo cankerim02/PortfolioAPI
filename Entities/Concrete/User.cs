d7511155469af00f07a455b771c717a62145cfc2
cankerim02 <cankerim02@gmail.com>
2025-08-06 22:31:02 +0300

    User bilgileri için varlık sınıfı oluşturuldu.

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        // // ilk etapta düz, sonra hash yapılır
+        public string Password { get; set; }
+        //admin- user ayrımı rolü için
+        public string Role { get; set; }
+    }
+}
