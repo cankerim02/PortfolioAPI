c06cc99432c7708b52c9c1ce0c438e8ba739ea84
cankerim02 <cankerim02@gmail.com>
2025-08-07 22:32:38 +0300

    Kullanıcı bilgileri için metot imzası oluşturuldu.

---
+++ DataAccess/Abstract/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmailAndPasswordAsync(string email, string password);
+        Task<User> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+    }
+}
