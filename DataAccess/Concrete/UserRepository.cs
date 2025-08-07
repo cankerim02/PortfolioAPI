0ac5c431ff17055928043fa6c08f6397b9a5f357
cankerim02 <cankerim02@gmail.com>
2025-08-07 22:33:42 +0300

    kullanıcı repo sınıfı için oluşturulan metot imzaları implemente edildi ve gerekli veritabanı işlemleri yapıldı.

---
+++ DataAccess/Concrete/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByEmailAndPasswordAsync(string email, string password)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        // KULLANICI SILME GÜNCELLEME DE KOYABİLİRSİN.
+    }
+}
