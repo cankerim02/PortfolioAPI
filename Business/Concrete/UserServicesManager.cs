aa50c211c321f4da2cf8428221d6ae57e6071a15
cankerim02 <cankerim02@gmail.com>
2025-08-24 13:16:43 +0300

    User bilgilerinin eklenmesi için iş kuralı tanımlandı

---
+++ Business/Concrete/UserServicesManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserServicesManager : IUserServices
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserServicesManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> AddUserAsync(UserAddDto userDto)
+        {
+            var existingUser = await _userRepository.GetByEmailAsync(userDto.Email);
+            if (existingUser != null)
+                return false;
+
+            var user = new User
+            {
+                Email = userDto.Email,
+                Password = userDto.Password,
+                Role = userDto.Role,
+            };
+
+            await _userRepository.AddAsync(user);
+            return true;
+
+        }
+    }
+}
