58da339a8be25317808d90d17794e2a0eec43400
cankerim02 <cankerim02@gmail.com>
2025-08-25 21:27:15 +0300

    api katmanında kullanılmak için userdto bilgisi oluşturuldu.

---
+++ Entities/Dtos/UserAddDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class UserAddDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // "Admin" veya "User" olabilir
+    }
+
+}
