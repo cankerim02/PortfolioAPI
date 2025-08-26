ed7a0d30e63a332cfe507ef409f943c099ef1711
cankerim02 <cankerim02@gmail.com>
2025-08-26 22:18:14 +0300

    ContactController eklendi, mesaj gönderme endpoint'i yazıldı

---
+++ Portfolio.API/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactMessageRepository _repository;
+
+        public ContactController(IContactMessageRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMessage(ContactMessageDto dto)
+        {
+            var message = new ContactMessage
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Message = dto.Message,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            await _repository.AddContactAsync(message);
+            return Ok(new { success = true, message = "Mesaj gönderildi" });
+        }
+    }
+}
