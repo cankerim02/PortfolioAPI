05d9933acb56e544be620c369eb9f54b3a60b26b
cankerim02 <cankerim02@gmail.com>
2025-09-04 22:17:22 +0300

    Auth service için manager sınıfı oluşturuldu ve gerekli iş kuralı yazıldı.( kullanıcı bilgileri veritabanından çekilecek dönüş başarılı olursa token bilgisi dönecek.)

---
+++ Business/Concrete/AuthServicesManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Core.Helper.Abstract;
+using DataAccess.Abstract;
+using Entities.Dtos;
+
+namespace Business.Concrete
+{
+    public class AuthServicesManager : IAuthServices
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtHelper _jwtHelper;
+
+        public AuthServicesManager(IUserRepository userRepository, IJwtHelper jwtHelper)
+        {
+            _userRepository = userRepository;
+            _jwtHelper = jwtHelper;
+        }
+
+        public async Task<LoginResultDto> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _userRepository.GetByEmailAndPasswordAsync(loginDto.Email, loginDto.Password);
+
+            if(user is null)
+            {
+                return new LoginResultDto { Success = false, Message = "Geçersiz kullanıcı bilgileri" };
+
+            }
+
+            var token = _jwtHelper.CreateToken(user);
+
+            return new LoginResultDto
+            {
+                Success = true,
+                Message = "Giriş Başarılı",
+                Token = token
+            };
+
+        }
+    }
+}
