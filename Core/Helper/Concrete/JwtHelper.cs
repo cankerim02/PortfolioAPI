f8c38c1268674598e1788926508466758b2d19cc
cankerim02 <cankerim02@gmail.com>
2025-09-04 22:23:03 +0300

    JwtHelper sınıfı oluşturularak  içerisine kullanıcı için claim bilgileri ve config dosyasından okunacak şekilde jwt token bilgisi tanımlanarak handler edildi.

---
+++ Core/Helper/Concrete/JwtHelper.cs
@@ -0,0 +1,42 @@
+﻿using Core.Helper.Abstract;
+using Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Helper.Concrete
+{
+    public class JwtHelper : IJwtHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new[]
+            {
+               new Claim(ClaimTypes.Name, user.Email),
+               new Claim(ClaimTypes.Role, user.Role)
+           };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer : _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims : claims,
+                expires : DateTime.Now.AddHours(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
