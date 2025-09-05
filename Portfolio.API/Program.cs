8e72e7fabc4471641f156af22e5cad16cd9ba4cd
cankerim02 <cankerim02@gmail.com>
2025-09-05 22:17:47 +0300

    register işlemleri yapıldı, jwt schema bilgisi ile authentication işlemi yapıldı.

--- Portfolio.API/Program.cs
+++ Portfolio.API/Program.cs
@@ -1,8 +1,14 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.Helper.Abstract;
+using Core.Helper.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +25,18 @@ builder.Services.AddScoped<IProjectRepository, EfProjectRepository>();
 builder.Services.AddScoped<IEmailServices, EmailServicesManager>();
 builder.Services.AddScoped<IProjectServices, ProjectServicesManager>();
 
+builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
+builder.Services.AddScoped<IDashboardService, DashboardServiceManager>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserServices, UserServicesManager>();
+
+builder.Services.AddScoped<IAuthServices, AuthServicesManager>();
+builder.Services.AddScoped<IJwtHelper, JwtHelper>();
+
+
+
+
 
 // CORS Policy
 builder.Services.AddCors(options =>
@@ -30,6 +48,29 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader()
             .AllowAnyMethod();
         });
+});
+
+// Authentication Scheme and AddJwtBearer
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        RoleClaimType = ClaimTypes.Role 
+    };
 });
 
 builder.Services.AddControllers();
@@ -51,6 +92,9 @@ app.UseHttpsRedirection();
 //CORS POLÝCY
 app.UseCors("AllowAngularApp");
 
+
+// Authentication and Authorization added
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
