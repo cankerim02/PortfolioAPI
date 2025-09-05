ee82fcac3ce254944cea78973f75998c0b4e1b46
cankerim02 <cankerim02@gmail.com>
2025-09-05 22:10:46 +0300

    AuthController eklendi ve login endpoint yazıldı

---
+++ Portfolio.API/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthServices _authService;
+
+        public AuthController(IAuthServices authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+
+            if (!result.Success)
+                return Unauthorized(result.Message);
+
+            return Ok(result);
+        }
+    }
+}
