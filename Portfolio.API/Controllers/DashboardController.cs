f7495430bf361fa12af8499534de4e993a2d9b54
cankerim02 <cankerim02@gmail.com>
2025-09-06 17:06:59 +0300

    Dashboard controller oluşturuldu. Dashboard bilgilerindeki istatistikler ve takvim etkinlikleri  endpoint olarak ayarlandı.

---
+++ Portfolio.API/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard()
+        {
+            var data = await _dashboardService.GetDashboardDataAsync();
+            return Ok(data);
+        }
+
+        
+        // Takvim için yeni endpoint
+        [HttpGet("calendar-events")]
+        public async Task<IActionResult> GetCalendarEvents()
+        {
+            
+            var events = await _dashboardService.GetCalendarEventsAsync();
+            return Ok(events);
+        }
+    }
+}
