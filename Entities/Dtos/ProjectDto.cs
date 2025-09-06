2ea3c0ee7071444365400f77cf90ae22015ca39a
cankerim02 <cankerim02@gmail.com>
2025-09-06 16:56:10 +0300

    Takvim formatında eklenen projenin gösterilmesi için dto sınıf oluşturuldu

---
+++ Entities/Dtos/ProjectDto.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class ProjectDto:IDto
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+     
+    }
+
+}
