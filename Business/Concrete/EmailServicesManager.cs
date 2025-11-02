a078cd26dea46eab76e8348ee78af9fea5f74c21
cankerim02 <cankerim02@gmail.com>
2025-11-02 14:32:43 +0300

    SendGrid kullanıcak şekilde ayarlandı ve try catch bloğu eklenerek hataların tutulacağı servislog tablosu eklenerek revize edildi

--- Business/Concrete/EmailServicesManager.cs
+++ Business/Concrete/EmailServicesManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
 using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System.Net;
 using System.Net.Mail;
 
@@ -9,31 +12,38 @@ namespace Business.Concrete
     public class EmailServicesManager : IEmailServices
     {
         private readonly IConfiguration _config;
+        private readonly IServiceLogServices _serviceLogServices;
 
-        public EmailServicesManager(IConfiguration config)
+        public EmailServicesManager(IConfiguration config, IServiceLogServices serviceLogServices)
         {
             _config = config;
+            _serviceLogServices = serviceLogServices;
         }
 
         public async Task SendEmailAsync(string toEmail, string subject, string body)
         {
-            var smtpClient = new SmtpClient(_config["Smtp:Host"])
+            try
             {
-                Port = int.Parse(_config["Smtp:Port"]),
-                Credentials = new NetworkCredential(_config["Smtp:Username"], _config["Smtp:Password"]),
-                EnableSsl = true
-            };
+                var apiKey = _config["Smtp:SendGridApiKey"];
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress(_config["Smtp:From"], "Admin");
+                var to = new EmailAddress(toEmail);
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, body, body);
 
-            var mailMessage = new MailMessage
+                //Fire-and forget mantığı ile mail gönderimi burda başlatıyorum ama await ile bloklamıyorum
+                _ = client.SendEmailAsync(msg);
+            }
+            catch (Exception ex)
             {
-                From = new MailAddress(_config["Smtp:From"]),
-                Subject = subject,
-                Body = body,
-                IsBodyHtml = false
-            };
+                await _serviceLogServices.LogAsync(new ServiceLog
+                {
+                    Message = $"Mail gönderilemedi {ex.Message}",
+                    StackTrace = ex.StackTrace,
+                    CreatedAt = DateTime.UtcNow,
+                });
+            }
 
-            mailMessage.To.Add(toEmail);
-            await smtpClient.SendMailAsync(mailMessage);
         }
     }
 }
+
