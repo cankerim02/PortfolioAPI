7fe72547aca28f1e5e46366dffad0abacff72aa1
cankerim02 <cankerim02@gmail.com>
2025-11-02 14:33:26 +0300

    genel hata kayıtlarının tutulması için servis log metot imzası oluşturuldu.

---
+++ Business/Concrete/ServiceLogManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ServiceLogManager : IServiceLogServices
+    {
+        private readonly IServiceLogRepository _repo;
+        public ServiceLogManager(IServiceLogRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task LogAsync(ServiceLog log)
+        {
+           await _repo.AddAsync(log);
+        }
+    }
+}
