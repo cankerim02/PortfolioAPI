eb212f8e9efd38e4deb1268dca30fd02c189414e
cankerim02 <cankerim02@gmail.com>
2025-11-02 14:33:55 +0300

    servislog model oluşturuldu

---
+++ Entities/Concrete/ServiceLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class ServiceLog
+    {
+        public int Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string? StackTrace { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
